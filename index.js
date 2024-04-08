02ecb1d113431c16ed06657f9b7e5ee344903526
Deep Boda <deep.boda@brightchamps.com>
2024-04-08 12:43:15 +0530

    add condition

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-// Function to check if element is in viewport
-const isInViewPort = (elem) => {
-  const bounding = elem.getBoundingClientRect();
-  return (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth) &&
-    bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight)
-  );
-};
-
-// Debounce a function call
-const debounce = (func, delay) => {
-  let inDebounce;
-  return function () {
-    const context = this;
-    const args = arguments;
-    clearTimeout(inDebounce);
-    inDebounce = setTimeout(() => func.apply(context, args), delay);
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to check if element is in viewport
+  const isInViewPort = (elem) => {
+    const bounding = elem.getBoundingClientRect();
+    return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.right <=
+        (window.innerWidth || document.documentElement.clientWidth) &&
+      bounding.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight)
+    );
   };
-};
 
-// Get all the products
-const blocks = document.querySelectorAll(".block");
+  // Debounce a function call
+  const debounce = (func, delay) => {
+    let inDebounce;
+    return function () {
+      const context = this;
+      const args = arguments;
+      clearTimeout(inDebounce);
+      inDebounce = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
 
-// Function which will make the API call
-const getBlocks = function () {
-  blocks.forEach((block) => {
-    if (isInViewPort(block)) {
-      console.log(block.innerText);
-    }
-  });
-  console.log(" ");
-};
+  // Get all the products
+  const blocks = document.querySelectorAll(".block");
 
-// Assign the event listener
-window.addEventListener("scroll", debounce(getBlocks, 1000), false);
+  // Function which will make the API call
+  const getBlocks = function () {
+    blocks.forEach((block) => {
+      if (isInViewPort(block)) {
+        console.log(block.innerText);
+      }
+    });
+    console.log(" ");
+  };
+
+  // Assign the event listener
+  window.addEventListener("scroll", debounce(getBlocks, 1000), false);
+});
